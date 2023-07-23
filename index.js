d4e73d047d832256bff73b3c8067df479fa67db1
Phanindra babu <chandraphanindrababu@gmail.com>
2023-07-23 06:48:04 +0000

    calculator

---
+++ index.js
@@ -0,0 +1,33 @@
+let result = document.getElementById("result");
+let expression = "";
+
+function appendToResult(value) {
+  expression += value;
+  result.value = expression;
+}
+
+function appendOperator(operator) {
+  if (expression !== "" && !isNaN(expression[expression.length - 1])) {
+    expression += operator;
+    result.value = expression;
+  }
+}
+
+function calculateResult() {
+  try {
+    expression = eval(expression).toString();
+    result.value = expression;
+  } catch (error) {
+    result.value = "Error";
+  }
+}
+
+function clearResult() {
+  expression = "";
+  result.value = "";
+}
+
+function deleteLast() {
+  expression = expression.slice(0, -1);
+  result.value = expression;
+}
